e699b786b94d5bc87065b584e98640101fd2dec1
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2017-07-24 09:29:35 +0800

    Initial Project

---
+++ FRF_Form_test_20170724/FRF_Form_test/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FRF_Form_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart_mag = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_phs = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_mag)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_phs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart_mag
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_mag.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_mag.Legends.Add(legend1);
+            this.chart_mag.Location = new System.Drawing.Point(12, 12);
+            this.chart_mag.Name = "chart_mag";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart_mag.Series.Add(series1);
+            this.chart_mag.Series.Add(series2);
+            this.chart_mag.Size = new System.Drawing.Size(908, 383);
+            this.chart_mag.TabIndex = 0;
+            this.chart_mag.Text = "chart1";
+            // 
+            // chart_phs
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart_phs.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart_phs.Legends.Add(legend2);
+            this.chart_phs.Location = new System.Drawing.Point(12, 401);
+            this.chart_phs.Name = "chart_phs";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart_phs.Series.Add(series3);
+            this.chart_phs.Series.Add(series4);
+            this.chart_phs.Size = new System.Drawing.Size(908, 383);
+            this.chart_phs.TabIndex = 1;
+            this.chart_phs.Text = "chart_phs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 966);
+            this.Controls.Add(this.chart_phs);
+            this.Controls.Add(this.chart_mag);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart_mag)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_phs)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_mag;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_phs;
+    }
+}
+
---
+++ FRF_Form_test_20170724/FRF_Form_test/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+using Mechatronics.MathToolBox;
+using Mechatronics.SimCore.FrequencyResponse;
+using Mechatronics.SimCore.Object;
+using Mechatronics.FANUC;
+
+namespace FRF_Form_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //WATCH OUT:
+            //Definition of Modes has changed.
+            //VLoopModes adds additional 0Hz. mode.
+            //0Hz. mode. should be also defined in Mode object.
+
+            //Object name change: Parameter --> ServoPrm
+
+            //Add Motor object. (not important)
+
+
+            //Create Structure Nature Modes Object
+            List<Mode> VLoopModes = new List<Mode>();
+            CreateModes(VLoopModes);
+
+            //Set experiment Control Parameters 
+            ServoPrm P = SetParameters();
+            FANUCMotor Motor = new FANUCMotor(1.2, 0.012); 
+
+            //create Object
+            VelocityLoop VR = new VelocityLoop(P, Motor, VLoopModes);
+
+            //read FRF File from csv file, which is created by servoguide
+            //this data is considered as referance.
+            FRF[] Close_ref =  Tool.Read_ServoGuide_FRFdata("Frequency_Response_Axis-1_1_-_1000Hz.csv");
+
+           
+            //set freq interval. it should be identical with ref.
+            VR.IsFreqDataSameAsRef = true;
+            VR.SetCustomFreqInterval(Close_ref);
+
+
+            //caculate simulated FRF.
+            FRF[] Close_sim = VR.GetCloseLoop();
+            FRF[] Open_sim = VR.GetOpenLoop();
+
+            //draw simulated FRF of close loop system
+            DrawLine(Close_ref, 0);
+            DrawLine(Close_sim, 1);
+
+
+            //remove controller response from measurement data
+            //extract Structure response
+            FRF[] Struct_ref = Tool.ExtractStructureResponseFromVLoop(P, Motor, Close_ref);
+
+
+            //caculate simulated Structure FRF.
+            FRF[] Struct_sim = new FRF[Struct_ref.Length];
+            for (int i = 0; i < Struct_ref.Length; i++)
+            {
+                Complex Res = Tool.GetStructureResponse(Struct_ref[i].Freq, VLoopModes, "Velocity");
+                Struct_sim[i] = new FRF(Struct_ref[i].Freq, Res);
+            }
+           
+            //draw simulated FRF of structure
+            DrawLine(Struct_ref, 0);
+            DrawLine(Struct_sim, 1);
+
+        }
+
+
+        ServoPrm SetParameters()
+        {//value is from *.prm file (ServoGuide Parameter file)
+
+            ServoPrm P = new ServoPrm();
+            //P.FANUCs.HRVGain = 300;
+            //P.FANUCs.VelocityLoopGain = 100;
+            //P.JL = 0.00546;
+
+            P.KVI = 740.2203300818m;
+            P.KVP = 6.6915923521467m;
+
+            P.dTzoh = 0.001m;
+            P.Tc = 0.0003m;
+            P.IsHighCycle = true;
+            P.IsFullCloseLoop = true;
+
+            return P;
+        }
+        void CreateModes(List<Mode> VLoopModes)
+        {
+
+            Mode mode;
+
+
+            mode = new Mode();
+            mode.Freq = 0;
+            mode.Zeta = 0;
+            mode.Mass = 0.00546 + 0.012;
+
+            VLoopModes.Add(mode);
+
+            mode = new Mode();
+            mode.Freq = 55;
+            mode.Zeta = 0.1;
+            mode.Mass = 0.3;
+            VLoopModes.Add(mode);
+
+            mode = new Mode();
+            mode.Freq = 120;
+            mode.Zeta = 0.07;
+            mode.Mass = 0.12;
+            VLoopModes.Add(mode);
+
+            mode = new Mode();
+            mode.Freq = 315;
+            mode.Zeta = 0.1;
+            mode.Mass = 0.05;
+            VLoopModes.Add(mode);
+
+
+        }
+
+        void DrawLine(FRF[] FRFData, int Channel)
+        {
+            //X AXIS in log scale
+            chart_mag.ChartAreas[0].AxisX.IsLogarithmic = true;
+            chart_phs.ChartAreas[0].AxisX.IsLogarithmic = true;
+
+            //reset
+            chart_mag.Series[Channel].Points.Clear();
+            chart_phs.Series[Channel].Points.Clear();
+
+            for (int i = 0; i < FRFData.Length; i++)
+            {
+                chart_mag.Series[Channel].Points.AddXY(FRFData[i].Freq, Mechatronics.MathToolBox.TL.DB(FRFData[i].Mag));
+                chart_phs.Series[Channel].Points.AddXY(FRFData[i].Freq, FRFData[i].Phs);
+
+            }
+
+        }
+
+
+    }
+}
